4e5e22da3e3c49d4b68570b48a82669c02cf2e0b
Darshan <aravinddarshan13897@outlook.com>
2022-07-20 17:27:56 +0530

    Update constants.py

--- pubmed-processor/constants.py
+++ pubmed-processor/constants.py
@@ -1,6 +1,6 @@
 
 file_pattern = '*.xml.gz'
-storage_folder = './datastore/'
+storage_folder = 'datastore/'
 
 full_files_ftp_url = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/*'
 inc_files_ftp_url = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles/*'
