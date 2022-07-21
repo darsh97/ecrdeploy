8bf191c2bdff962c5197da8fd7e60ec5e5ace7c1
Darshan <aravinddarshan13897@outlook.com>
2022-07-21 19:14:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,59 +10,69 @@ from constants import storage_folder, s3_destination_bucket
 
 from helper import upload_file
 
+from random import shuffle
+
 
 def main():
-    # fetch_from_ftp_to_local: bool = ftp_puller()
-    # assert bool(fetch_from_ftp_to_local)
+    print("Starting to fetch data", flush=True)
 
-    for data_file in os.listdir(storage_folder):
+    #fetch_from_ftp_to_local: bool = ftp_puller()
+
+    print("Download Complete\n", flush=True)
+    print(os.listdir(storage_folder), flush=True)
+
+    downloaded_files = os.listdir(storage_folder)
+
+    shuffle(downloaded_files)
+
+    for data_file in downloaded_files:
 
         if data_file.endswith(".gz"):
 
             article_data = []
 
             data_file_local_path: str = os.path.join(storage_folder, data_file)
-            print(f"Processing {data_file}")
+            print(f"Processing {data_file}", flush=True)
+
+            print(f"Extracting {data_file}", flush=True)
+            _zip = gzip.open(data_file_local_path, "r")
+            print(f"Extracted {data_file}", flush=True)
 
             try:
-                print(f"Extracting {data_file}")
+                _zip.read()
+            except Exception as e:
+                print(f"Error {data_file}")
+                continue
 
-                with gzip.open(data_file_local_path, "r") as _zip:
-                    _xml = bs(_zip.read(), "lxml")
+            _xml = bs(_zip.read(), "lxml")
 
-                print(f"Parsing {data_file}")
+            print(f"Parsing {data_file}", flush=True)
 
-                for article in _xml.findAll("pubmedarticle"):
-                    article_data.append(parse_article(article))
+            for article in _xml.findAll("pubmedarticle"):
+                article_data.append(parse_article(article))
 
-                print(f"Converting {data_file} to Dataframe!")
-                article_df = pd.DataFrame(article_data)
+            print(f"Converting {data_file} to Dataframe!", flush=True)
+            article_df = pd.DataFrame(article_data)
 
-                xlsx_file_name: str = data_file.split(".")[0] + ".xlsx"
-                pkl_file_name: str = data_file.split(".")[0] + ".pkl"
+            xlsx_file_name: str = data_file.split(".")[0] + ".xlsx"
+            pkl_file_name: str = data_file.split(".")[0] + ".pkl"
 
-                xlsx_local_file_path: str = os.path.join(storage_folder, xlsx_file_name)
-                pkl_local_file_path: str = os.path.join(storage_folder, pkl_file_name)
+            xlsx_local_file_path: str = os.path.join(storage_folder, xlsx_file_name)
+            pkl_local_file_path: str = os.path.join(storage_folder, pkl_file_name)
 
-                print(f"Converting {data_file}_dataframe to Pickle: {pkl_local_file_path}")
-                article_df.to_pickle(pkl_local_file_path)
+            print(f"Converting {data_file}_dataframe to Pickle: {pkl_local_file_path}", flush=True)
+            article_df.to_pickle(pkl_local_file_path)
 
-                print(f"Converting {data_file}_dataframe to Excel: {xlsx_local_file_path}")
-                article_df.to_excel(xlsx_local_file_path)
+            print(f"Converting {data_file}_dataframe to Excel: {xlsx_local_file_path}", flush=True)
+            article_df.to_excel(xlsx_local_file_path)
 
-                if os.path.isfile(pkl_local_file_path):
-                    upload_response = upload_file(pkl_local_file_path, s3_destination_bucket, pkl_file_name)
-                    print(f"Uploaded {pkl_file_name} to {s3_destination_bucket}")
+            if os.path.isfile(pkl_local_file_path):
+                upload_response = upload_file(pkl_local_file_path, s3_destination_bucket, pkl_file_name)
+                print(f"Uploaded {pkl_file_name} to {s3_destination_bucket}", flush=True)
 
-                if os.path.isfile(xlsx_local_file_path):
-                    upload_response = upload_file(xlsx_local_file_path, s3_destination_bucket, xlsx_file_name)
-                    print(f"Uploaded {xlsx_file_name} to {s3_destination_bucket}")
-
-
-            except Exception as err:
-                print(f"{data_file}: {err}")
-
-        return
+            if os.path.isfile(xlsx_local_file_path):
+                upload_response = upload_file(xlsx_local_file_path, s3_destination_bucket, xlsx_file_name)
+                print(f"Uploaded {xlsx_file_name} to {s3_destination_bucket}", flush=True)
 
 
 if __name__ == '__main__':
