035018aab5c7a1e12f971148e1d1d44b1fbc2217
Darshan <aravinddarshan13897@outlook.com>
2022-07-23 12:20:51 +0530

    Update constants.py

--- pubmed-processor/constants.py
+++ pubmed-processor/constants.py
@@ -1,9 +1,8 @@
-
-file_pattern = 'pubmed22n1191.xml.gz'
+file_pattern = '*.xml.gz'
 storage_folder = 'datastore/'
 
-full_files_ftp_url = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/*'
+full_files_ftp_url = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline'
 inc_files_ftp_url = 'ftp://ftp.ncbi.nlm.nih.gov/pubmed/updatefiles'
 
-
-s3_destination_bucket = "reqpayertest"
+s3_destination_bucket = "codebucket234"
+processed_file_log: str = "completed_files.txt"
